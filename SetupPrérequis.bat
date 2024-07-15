0cacf3af0de0095e7b3ca0ff21dab42445cc2eb4
Mag-jpg <maglesinge@gmail.com>
2024-07-15 14:07:29 +0200

    Add files via upload

---
+++ SetupPrérequis.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Updating pip...
+python -m pip install --upgrade pip
+
+echo Installing required libraries...
+pip install numpy matplotlib
+
+echo All requirements installed.
+pause
